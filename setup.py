f64426c5f41dd53adf90c8fbe0868a563c5eeb03
Etienne Millon <me@emillon.org>
2015-01-28 16:43:39 +0100

    add license & stuff

--- setup.py
+++ setup.py
@@ -11,8 +11,19 @@ with open('HISTORY.rst') as f:
 
 setuptools.setup(name='mixcloud',
                  version='0.0+dev',
+                 author='Etienne Millon',
+                 author_email='me@emillon.org',
+                 url="https://github.com/emillon/mixcloud",
                  packages=['mixcloud'],
                  install_requires=reqs,
                  description='Bindings for the mixcloud.com API',
                  long_description=readme + '\n\n' + history,
+                 classifiers=[
+                     'Development Status :: 3 - Alpha'
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: BSD License',
+                     'Operating System :: OS Independent',
+                     'Programming Language :: Python :: 2',
+                     'Programming Language :: Python :: 3',
+                     ],
                  )
