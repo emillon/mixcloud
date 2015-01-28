b1f52ee8e5bbbc429d20338c4d17a8dc68173c00
Etienne Millon <me@emillon.org>
2015-01-28 16:36:20 +0100

    Add HISTORY.rst

--- setup.py
+++ setup.py
@@ -3,8 +3,16 @@ import setuptools
 
 reqs = [str(ir.req) for ir in parse_requirements('requirements.txt')]
 
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('HISTORY.rst') as f:
+    history = f.read()
+
 setuptools.setup(name='mixcloud',
                  version='0.0+dev',
                  packages=['mixcloud'],
                  install_requires=reqs,
+                 description='Bindings for the mixcloud.com API',
+                 long_description=readme + '\n\n' + history,
                  )
