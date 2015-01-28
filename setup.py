fc979e3f11fd523df002e01b080f5f50136de5ab
Etienne Millon <me@emillon.org>
2015-01-28 17:09:52 +0100

    back to business

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('HISTORY.rst') as f:
     history = f.read()
 
 setuptools.setup(name='mixcloud',
-                 version='0.0.1',
+                 version='0.0.1+dev',
                  author='Etienne Millon',
                  author_email='me@emillon.org',
                  url="https://github.com/emillon/mixcloud",
