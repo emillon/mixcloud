2c2f0861849a146cda0e48c0a420d509edeab59e
Ben Tappin <ben@mrben.co.uk>
2015-10-11 16:57:08 +0100

    Don't bail if the netrc lookup fails. We might not even be using it.

--- mixcloud/__init__.py
+++ mixcloud/__init__.py
@@ -75,10 +75,16 @@ class Mixcloud(object):
     def __init__(self, api_root=API_ROOT, access_token=None):
         self.api_root = api_root
         if access_token is None:
-            # Attempt netrc lookup.
-            netrc_auth = netrc.netrc().authenticators(NETRC_MACHINE)
-            if netrc_auth:
-                access_token = netrc_auth[2]
+            try:
+                # Attempt netrc lookup.
+                netrc_auth = netrc.netrc().authenticators(NETRC_MACHINE)
+                if netrc_auth:
+                    access_token = netrc_auth[2]
+            except netrc.NetrcParseError:
+                # Configuration errors unrelated to the Mixcloud entry will
+                # cause this exception to be thrown, whether or not there is a
+                # Mixcloud entry.
+                pass
         self.access_token = access_token
 
     def artist(self, key):
